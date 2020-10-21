bc52206160262a5095110d8d2095fb0e5358b6dd
Ali Mousavi Kherad <alimousavikherad@gmail.com>
2020-10-21 15:53:43 +0330

    Added TypeServerExpress

--- src/server/TypeServerExpress.ts
+++ src/server/TypeServerExpress.ts
@@ -1,8 +1,22 @@
+import {IServerInfrastructure, EndpointInfo} from '@type-server/typeserver';
 
-class TypeServerExpressServer {
+class TypeServerExpressServer implements IServerInfrastructure {
 
+    name(): string {
+        return "express";
+    }
 
+    listen(endpoint: EndpointInfo): void {
+        
+    }
 
+    async start(): Promise<void> {
+        
+    }
+
+    async stop(): Promise<void> {
+        
+    }
 }
 
 export const TypeServerExpress = new TypeServerExpressServer();
